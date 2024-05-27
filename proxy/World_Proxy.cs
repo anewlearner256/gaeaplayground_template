d34c4d797ac0528337798cd6d8dc1cdbf7f6f0a7
ning_xue <1561079334@qq.com>
2024-05-27 11:12:31 +0000

    ✨feature: 增加生成网页接口的代码

--- proxy/World_Proxy.cs
+++ proxy/World_Proxy.cs
@@ -5,8 +5,10 @@
 using Godot;
 using Gaea;
 using Gaea.Scene;
+using Gaea.Export;
 public class World_Proxy : World
 {
+    internal readonly Godot.Directory DIRECTORY = new Godot.Directory();
     public override void _Ready()
     {
         GD.Print($"当前引擎版本: [{GetVersion()}]");
@@ -17,10 +19,22 @@ public class World_Proxy : World
 
         // DrawDoubleArrowTool drawDoubleArrowTool = new DrawDoubleArrowTool();
         // World.Instance.AddChild(drawDoubleArrowTool);
-		// drawDoubleArrowTool.OverDraw += () =>
-		// {
-			
-		// };
+        // drawDoubleArrowTool.OverDraw += () =>
+        // {
+
+        // };
+
+        var code = ExportApiManager.Instance.Export(Language.JavaScript);
+        var path = "res://exports/html5";
+        if (!DIRECTORY.DirExists(path))
+            DIRECTORY.MakeDirRecursive(path);
+        using (var f = new File())
+        {
+            f.Open($"{path}/index.js", File.ModeFlags.Write);
+            f.StoreString(code);
+        }
+
+        
 
     }
 
